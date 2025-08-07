adb1c18295d90ec111005d1c199493119c4423c9
Jawad Ahmad <116241553+Jawadahmad9938@users.noreply.github.com>
2025-08-07 13:35:09 +0500

    Add files via upload

---
+++ input.js
@@ -0,0 +1,9 @@
+const inputData = 
+{
+    "gameList": [
+      { "id": "game1", "title": "Alphabet Matching Game", "instructions": "Drag and drop the letters to the correct boxes." },
+      { "id": "game2", "title": "Math Puzzle Challenge", "instructions": "Solve the puzzle using arithmetic skills." },
+      { "id": "game3", "title": "Color Sorter", "instructions": "Sort the colors into the correct buckets." },
+      { "id": "game4", "title": "Shape Matcher", "instructions": "Match shapes to their corresponding slots." }
+    ]
+  }
