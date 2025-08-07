44099e914aadb5448a6fde233578bbad60b3356f
jawad <jawadahmad9938@gmail.com>
2025-08-07 14:10:15 +0500

    new

---
+++ games/game1/input.js
@@ -0,0 +1,109 @@
+export const inputData = 
+{
+  "mainBoard": [
+    {
+      "shape": "parallelogram",
+      "d": "M1,60 L35,122 L104,0 L35,2 Z",
+      "viewBox": "-30 -30 164 182",
+      "width": "104",
+      "height": "122",
+      "position": {
+        "top": "170px",
+        "left": "263.6666564941406px"
+      },
+      "rotation": 0,
+      "fill": "#EE1B24"
+    },
+    {
+      "shape": "parallelogram",
+      "d": "M1,60 L35,122 L104,0 L35,2 Z",
+      "viewBox": "-30 -30 164 182",
+      "width": "104",
+      "height": "122",
+      "position": {
+        "top": "192px",
+        "left": "295.6666564941406px"
+      },
+      "rotation": 0,
+      "fill": "none"
+    },
+    {
+      "shape": "diamond",
+      "d": "M33,0L0,61.000004 37,116 71,59 33,1z",
+      "viewBox": "-30 -30 131 176",
+      "width": "71",
+      "height": "116",
+      "position": {
+        "top": "183px",
+        "left": "364.1666564941406px"
+      },
+      "rotation": 150,
+      "fill": "none"
+    },
+    {
+      "shape": "smallTriangle",
+      "d": "M32,1L0,58 67,58 33,0z",
+      "viewBox": "-30 -30 127 118",
+      "width": "67",
+      "height": "58",
+      "position": {
+        "top": "272.3333330154419px",
+        "left": "308.1666564941406px"
+      },
+      "rotation": 60,
+      "fill": "none"
+    },
+    {
+      "shape": "square",
+      "d": "M0,0L73,0 73,70 0,70z",
+      "viewBox": "-30 -30 133 130",
+      "width": "73",
+      "height": "70",
+      "position": {
+        "top": "318.33333349227905px",
+        "left": "301.1666564941406px"
+      },
+      "rotation": 45,
+      "fill": "none"
+    },
+    {
+      "shape": "hex",
+      "d": "M35,0 L1,62 36,122 37,122 103,122 137,60 103,1 L35,0 Z",
+      "viewBox": "-30 -30 197 182",
+      "width": "137",
+      "height": "122",
+      "position": {
+        "top": "254.33334350585938px",
+        "left": "347.1666564941406px"
+      },
+      "rotation": 30,
+      "fill": "none"
+    },
+    {
+      "shape": "uniqueDiamond",
+      "d": "M42.3543 63.1061L53 2L11.2362 50.7197L1 111L42.3543 63.1061Z",
+      "viewBox": "-30 -30 114 173",
+      "width": "54",
+      "height": "113",
+      "position": {
+        "top": "332.16668701171875px",
+        "left": "316.6666564941406px"
+      },
+      "rotation": 30,
+      "fill": "none"
+    },
+    {
+      "shape": "parallelogram",
+      "d": "M1,60 L35,122 L104,0 L35,2 Z",
+      "viewBox": "-30 -30 164 182",
+      "width": "104",
+      "height": "122",
+      "position": {
+        "top": "250px",
+        "left": "414.6666564941406px"
+      },
+      "rotation": 150,
+      "fill": "none"
+    }
+  ]
+}
