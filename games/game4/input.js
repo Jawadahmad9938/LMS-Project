5d4396898e79659dc0a8c11c85cbdbfcfab3c9e4
jawad <jawadahmad9938@gmail.com>
2025-08-07 14:22:47 +0500

    new game add

---
+++ games/game4/input.js
@@ -0,0 +1,15 @@
+const inputData =
+{
+  "background": {
+    "name": "/games/game4/BackgroundImage2.jpg"
+  },
+  "objectImage": {
+    "name": "/games/game4/Objectimage.png"
+  },
+  "clickSound": {
+    "name": "/games/game4/beep-329314.mp3"
+  },
+  "totalObjects": 20,
+  "correctObjects": 5,
+  "speed": 5
+}
