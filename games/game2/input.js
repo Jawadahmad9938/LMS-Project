ca3757da156c4586ce304c2e9ef2bf09e4756841
jawad <jawadahmad9938@gmail.com>
2025-08-07 14:20:44 +0500

    new changes

---
+++ games/game2/input.js
@@ -0,0 +1,11 @@
+// input.js
+const InputData =
+{
+    pdfFiles: [
+      "Visual Programming Project Report Inventory MS.pdf",
+      "Visual Programming Project Report Event MS.pdf",
+      "Umair Ayan Akbar CV.pdf",
+      "Tic_Tac_Toe_DQN_Report.pdf"
+    ]
+}
+  
---
+++ games/game3/input.js
@@ -0,0 +1,17 @@
+const InputData = 
+{
+  "background": "assets/background image7.jpg",
+  "objectImage": "assets/Objectimage3.png",
+  "sounds": {
+    "rotate": "assets/beep-329314.mp3",
+    "detection": "assets/swoosh-09-352859 (mp3cut.net).mp3",
+    "win": "assets/congratulations-male-spoken-264675 (mp3cut.net).mp3"
+  },
+  "settings": {
+    "numObjects": 10,
+    "objectSize": 100,
+    "showDuration": 1000,
+    "interval": 5000
+  }
+}
+  
