14acfeeb4903fee22a561286a6517c5b0837ebf8
jawad <jawadahmad9938@gmail.com>
2025-08-08 01:36:55 +0500

    changings

--- games/game5/input.js
+++ games/game5/input.js
@@ -4,8 +4,8 @@ const inputData =
   "gridSize": "4x4",
   "pairType": "different",
   "backImage": "A dark-themed logo for Lab D, representing an IT company with a sleek and modern design.png",
-  "wrongSound": "nien.mp3",
-  "correctSound": "question.mp3",
+  "wrongSound": "./nien.mp3",
+  "correctSound": "./question.mp3",
   "timer": true,
   "timerDuration": 90,
   "replayOption": "shuffle",
@@ -13,89 +13,89 @@ const inputData =
   "stages": {
     "P1": {
       "first": {
-        "image": "5c1e1ee94fdbba581bb8e232_Titan_Objects.jpg",
-        "audio": "beep.mp3"
+        "image": "./5c1e1ee94fdbba581bb8e232_Titan_Objects.jpg",
+        "audio": "./beep.mp3"
       },
       "second": {
-        "image": "5c1e1ee94fdbba581bb8e232_Titan_Objects.jpg",
-        "audio": "beep.mp3"
+        "image": "./5c1e1ee94fdbba581bb8e232_Titan_Objects.jpg",
+        "audio": "./beep.mp3"
       },
       "correctAudio": ""
     },
     "P2": {
       "first": {
-        "image": "5c1e1ee9753a4011e0104d55_Titan.jpg",
-        "audio": "beep.mp3"
+        "image": "./5c1e1ee9753a4011e0104d55_Titan.jpg",
+        "audio": "./beep.mp3"
       },
       "second": {
-        "image": "5c1e1ee9753a4011e0104d55_Titan.jpg",
-        "audio": "beep.mp3"
+        "image": "./5c1e1ee9753a4011e0104d55_Titan.jpg",
+        "audio": "./beep.mp3"
       },
       "correctAudio": ""
     },
     "P3": {
       "first": {
-        "image": "img1.jpg",
-        "audio": "beep.mp3"
+        "image": "./img1.jpg",
+        "audio": "./beep.mp3"
       },
       "second": {
-        "image": "img1.jpg",
-        "audio": "beep.mp3"
+        "image": "./img1.jpg",
+        "audio": "./beep.mp3"
       },
       "correctAudio": ""
     },
     "P4": {
       "first": {
-        "image": "img2.jpg",
-        "audio": "beep.mp3"
+        "image": "./img2.jpg",
+        "audio": "./beep.mp3"
       },
       "second": {
-        "image": "img2.jpg",
-        "audio": "beep.mp3"
+        "image": "./img2.jpg",
+        "audio": "./beep.mp3"
       },
       "correctAudio": ""
     },
     "P5": {
       "first": {
-        "image": "img3.jpg",
-        "audio": "beep.mp3"
+        "image": "./img3.jpg",
+        "audio": "./beep.mp3"
       },
       "second": {
-        "image": "img3.jpg",
-        "audio": "beep.mp3"
+        "image": "./img3.jpg",
+        "audio": "./beep.mp3"
       },
       "correctAudio": ""
     },
     "P6": {
       "first": {
-        "image": "img4.jpg",
-        "audio": "beep.mp3"
+        "image": "./img4.jpg",
+        "audio": "./beep.mp3"
       },
       "second": {
-        "image": "img4.jpg",
-        "audio": "beep.mp3"
+        "image": "./img4.jpg",
+        "audio": "./beep.mp3"
       },
       "correctAudio": ""
     },
     "P7": {
       "first": {
-        "image": "img5.jpg",
-        "audio": "beep.mp3"
+        "image": "./img5.jpg",
+        "audio": "./beep.mp3"
       },
       "second": {
-        "image": "img5.jpg",
-        "audio": "beep.mp3"
+        "image": "./img5.jpg",
+        "audio": "./beep.mp3"
       },
       "correctAudio": ""
     },
     "P8": {
       "first": {
-        "image": "img6.jpg",
-        "audio": "beep.mp3"
+        "image": "./img6.jpg",
+        "audio": "./beep.mp3"
       },
       "second": {
-        "image": "img6.jpg",
-        "audio": "beep.mp3"
+        "image": "./img6.jpg",
+        "audio": "./beep.mp3"
       },
       "correctAudio": ""
     }
