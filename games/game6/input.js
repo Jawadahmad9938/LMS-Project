86a0b22a4674cc9e1cc4d9ca1b4eb1936d43d406
jawad <jawadahmad9938@gmail.com>
2025-08-07 14:25:55 +0500

    game6 add

---
+++ games/game6/input.js
@@ -0,0 +1,86 @@
+const inputData = 
+{
+    "slides": [
+        {
+            "letters": [
+                {
+                    "value": "אָ",
+                    "type": "letter"
+                },
+                {
+                    "value": "בּ",
+                    "type": "letter"
+                },
+                {
+                    "value": "ג",
+                    "type": "fixed"
+                }
+            ]
+        },
+        {
+            "letters": [
+                {
+                    "value": "אָ",
+                    "type": "letter"
+                },
+                {
+                    "value": "בּ",
+                    "type": "letter"
+                },
+                {
+                    "value": "ג",
+                    "type": "fixed"
+                }
+            ]
+        },
+        {
+            "letters": [
+                {
+                    "value": "בּ",
+                    "type": "letter"
+                },
+                {
+                    "value": "אָ",
+                    "type": "letter"
+                },
+                {
+                    "value": "ג",
+                    "type": "fixed"
+                }
+            ]
+        },
+        {
+            "letters": [
+                {
+                    "value": "ג",
+                    "type": "fixed"
+                },
+                {
+                    "value": "בּ",
+                    "type": "letter"
+                },
+                {
+                    "value": "אָ",
+                    "type": "letter"
+                }
+            ]
+        },
+        {
+            "letters": [
+                {
+                    "value": "ג",
+                    "type": "fixed"
+                },
+                {
+                    "value": "אָ",
+                    "type": "letter"
+                },
+                {
+                    "value": "בּ",
+                    "type": "letter"
+                }
+            ]
+        }
+    ],
+    "qaudio": "question.mp3"
+}
