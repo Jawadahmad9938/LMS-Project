0b82605fcf7883781cd88f7f32b7eb6b3f91acf1
jawad <jawadahmad9938@gmail.com>
2025-08-11 12:14:40 +0500

    new updates in game3

--- games/game3/input.js
+++ games/game3/input.js
@@ -1,17 +1,16 @@
-const InputData = 
-{
-  "background": "assets/background image5.jpg",
-  "objectImage": "assets/Objectimage3.png",
-  "sounds": {
-    "rotate": "assets/beep-329314.mp3",
-    "detection": "assets/swoosh-09-352859 (mp3cut.net).mp3",
-    "win": "assets/congratulations-male-spoken-264675 (mp3cut.net).mp3"
+const inputData = {
+  background: "assets/background image9.jpg",
+  objectImage: "assets/Object4.png",
+  correctObjectImage: "assets/rabbitObject.png",
+  sounds: {
+    rotate: "assets/swoosh-09-352859 (mp3cut.net).mp3",
+    detection: "assets/notification-sound-effect-372475 (mp3cut.net).mp3",
+    win: "assets/congratulations-male-spoken-264675 (mp3cut.net).mp3",
   },
-  "settings": {
-    "numObjects": 10,
-    "objectSize": 100,
-    "showDuration": 1000,
-    "interval": 5000
-  }
-}
-  
+  settings: {
+    numObjects: 6,
+    objectSize: 100,
+    showDuration: 1000,
+    interval: 5000,
+  },
+};
